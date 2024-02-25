9f3acb531ea87c10979e8400cdff86ec6c2539fd
Tuhin Kundu <tuhinkundu@outlook.com>
2024-02-25 10:44:12 -0800

    fix logging bugs, improve ckpting

--- run_mlm.py
+++ run_mlm.py
@@ -15,12 +15,11 @@ from torch.utils.data import DataLoader
 import argparse
 import multiprocessing, os
 import accelerate
-from accelerate.logging import get_logger
 import math
 import wandb
 from tqdm.auto import tqdm
 from decimal import Decimal
-import datetime
+import datetime, json
 
 class MLMTrainer:
     def __init__(self, tokenizer, model, args):
@@ -31,13 +30,13 @@ class MLMTrainer:
         self.lr_scheduler = get_scheduler("linear",optimizer=self.optimizer,num_warmup_steps=self.args.warmup_steps,
                                           num_training_steps=self.args.total_steps)
         self.train_dataloader, self.val_dataloader = self.load_data(self.args.data_path, self.args.batch_size, self.args.num_proc)
-        self.accelerator = accelerate.Accelerator(split_batches=True, log_with='wandb') #grad acc and precision (mixed/bf16) in deepspeed config
+        self.accelerator = accelerate.Accelerator(split_batches=True) #grad acc and precision (mixed/bf16) in deepspeed config
         self.model, self.optimizer, self.lr_scheduler, self.train_dataloader, self.val_dataloader =  self.accelerator._prepare_deepspeed(
             self.model, self.optimizer, self.lr_scheduler, self.train_dataloader, self.val_dataloader
         )
         self.experiment_name = (f'{self.args.model_name}_mrl{self.args.mrl}{self.args.mrl_efficient}_'
                                 f'bs{self.args.batch_size}_lr{"%.1E"%Decimal(self.args.lr)}_warmup{self.args.warmup_steps}')
-        self.logger = get_logger(self.experiment_name)
+
 
 
     def load_data(self, data_path, batch_size, num_proc=16):
@@ -51,28 +50,47 @@ class MLMTrainer:
         val_dataloader = DataLoader(dataset['test'], batch_size=batch_size, shuffle=True, num_workers=num_proc//4)
         return train_dataloader, val_dataloader
 
-    def print_time_log(self, progress_bar):
+    def print_time_log(self, iter, progress_bar):
         time_elapsed = progress_bar.format_dict['elapsed']
         rate = progress_bar.format_dict["rate"]
         remaining = (progress_bar.total - progress_bar.n) / rate if rate and progress_bar.total else 0
-        self.logger.info(
-            f'Iterations: {iter} elapsed time: {datetime.timedelta(seconds=time_elapsed)} '
-            f'and remaining: {datetime.timedelta(seconds=remaining)}', main_process_only=True)
+        print(f'Iterations {iter} ; elapsed time: {datetime.timedelta(seconds=time_elapsed)} and remaining: {datetime.timedelta(seconds=remaining)}')
 
     def train(self):
 
         self.model.train()
-        progress_bar = tqdm(range(self.args.total_steps))
 
-        wandb_config = {'learning_rate':self.args.lr}
-        wandb_run = wandb.init(project=self.experiment_name,
-                    config=wandb_config)
+
+
+
         self.accelerator.register_for_checkpointing(self.lr_scheduler)
-        self.logger.info(str(self.args))
+        print(self.args)
         out_dir = f'{self.args.output_path}/{self.experiment_name}'
         os.makedirs(out_dir, exist_ok=True)
 
-        for iter, batch in zip(range(self.args.total_steps), self.train_dataloader):
+        if self.args.resume_train_from:
+            self.accelerator.load_state(self.args.resume_train_from)
+            with open(f'{out_dir}/args.json', 'r') as f:
+                args = json.load(f)
+            init_step = args['iter']
+            self.args.__dict__ = args
+            wandb_config = {'learning_rate': self.args.lr}
+            wandb_run = wandb.init(project=self.experiment_name,
+                                   config=wandb_config)
+            self.args.wandb_runid = wandb.run.id
+
+        else:
+            with open(f'{out_dir}/args.json', 'w') as f:
+                json.dump(self.args.__dict__, f)
+            init_step = 0
+
+            wandb_config = {'learning_rate': self.args.lr}
+            wandb_run = wandb.init(project=self.experiment_name,
+                                   config=wandb_config)
+            self.args.wandb_runid = wandb.run.id
+
+        progress_bar = tqdm(range(init_step, self.args.total_steps))
+        for iter, batch in zip(range(init_step, self.args.total_steps), self.train_dataloader):
 
             outputs = model(**batch)
             loss = outputs.loss
@@ -84,14 +102,24 @@ class MLMTrainer:
             if self.accelerator.is_main_process:
 
                 wandb.log({'loss': loss.item(), 'learning_rate': self.optimizer.param_groups[0]['lr']})
+                self.args.iter = iter
+
+                if iter % (self.args.evaluation_interval//4) ==0:
+                    self.print_time_log(iter, progress_bar)
+
             self.model.eval()
 
-            if iter % self.args.evaluation_interval==0 and iter>1:
+            if iter % self.args.evaluation_interval==0 and iter>self.args.evaluation_interval-1: #iter so that loaded ckpt doesn't get saved again immediately
                 losses = []
                 eval_progress = tqdm(range(len(self.val_dataloader)))
                 for step, batch in enumerate(self.val_dataloader):
+                    if step==10:
+                        break
                     with torch.no_grad():
                         outputs = self.model(**batch)
+                        loss = outputs.loss
+                        losses.append(self.accelerator.gather(loss.repeat(batch['input_ids'].shape[0])))
+                        eval_progress.update(1)
 
 
                 self.accelerator.wait_for_everyone()
@@ -102,16 +130,29 @@ class MLMTrainer:
                     mean_loss = torch.mean(losses)
                     perplexity = math.exp(mean_loss)
                     wandb.log({'perplexity': perplexity, 'val_loss': mean_loss.item()})
-                    self.print_time_log(progress_bar)
+                    self.print_time_log(iter, progress_bar)
 
                     #unwrap_model = self.accelerator.unwrap_model(self.model)
                     #unwrap_model.save_pretrained(weight_dir, save_function = self.accelerator.save)
 
                     self.accelerator.save_state(out_dir)
+
+                    self.args.lr = self.optimizer.param_groups[0]['lr']
                     artifact = wandb.Artifact(name=self.experiment_name, type='model')
                     artifact.add_dir(out_dir)
-
                     wandb_run.log_artifact(artifact)
+
+                    with open(f'{out_dir}/args.json', 'w') as f:
+                        json.dump(self.args.__dict__, f)
+
+
+        self.accelerator.save_state(out_dir)
+        artifact = wandb.Artifact(name=self.experiment_name, type='model')
+        artifact.add_dir(out_dir)
+        wandb_run.log_artifact(artifact)
+
+        with open(f'{out_dir}/args.json', 'w') as f:
+            json.dump(self.args.__dict__, f)
 
 
 if __name__ == '__main__':
@@ -130,18 +171,18 @@ if __name__ == '__main__':
         return dims.split[',']
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, help='path to processed mlm dataset', default='bert/bookcorpus_train')
-    parser.add_argument('--output_path', type=str, help='path to save logs and model weights', default='output_dir')
+    parser.add_argument('--data_path', type=str, help='path to processed mlm dataset', default='/tk/bert/bookcorpus_train')
+    parser.add_argument('--output_path', type=str, help='path to save logs and model weights', default='/tk/output_dir')
     parser.add_argument('--resume_train_from', type=str, help='path to saved checkpoint to resume training', default=None)
     parser.add_argument('--model_name', type=str, default='bert-base-uncased')
     parser.add_argument('--mrl', type=str2bool, default=False, help='whether to use MRL')
     parser.add_argument('--mrl_efficient', type=str2bool, default=False)
     parser.add_argument('--nesting_dim', type=str2list, default=[96,192,384,768])
-    parser.add_argument('--batch_size', type=int, default=4, help='total batch size across multiple gpus/nodes')
+    parser.add_argument('--batch_size', type=int, default=256, help='total batch size across multiple gpus/nodes')
     parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
     parser.add_argument('--total_steps', type=int, default=1000000)
     parser.add_argument('--weight_decay', type=float, default=0.01)
-    parser.add_argument('--warmup_steps', type=int, default=2000)
+    parser.add_argument('--warmup_steps', type=int, default=10000)
     parser.add_argument('--num_proc', type=int, default=multiprocessing.cpu_count(), help='number of processes')
     parser.add_argument('--evaluation_interval', type=int, default=25000)
     parser.add_argument('--wandb_key', type=str, default=None, help='wandb api login key for remote login')
@@ -164,11 +205,8 @@ if __name__ == '__main__':
     model = BertForMaskedLM(config=config)
 
 
-
-
     trainer = MLMTrainer(tokenizer, model, args)
     trainer.train()
-
 
 
 
