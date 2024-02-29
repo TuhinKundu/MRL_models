39c199e7a6ae1e3c42e9cd42b611820340112cbf
Tuhin Kundu <tuhinkundu@outlook.com>
2024-02-28 19:15:42 -0800

    push open_clip integration and training

--- run_pretraining.py
+++ run_pretraining.py
@@ -6,6 +6,7 @@ References
 4. https://huggingface.co/docs/transformers/main/en/tasks/masked_language_modeling
 '''
 from datasets import load_from_disk
+import datasets
 from transformers import AutoTokenizer, AutoConfig
 from bert.modeling_bert import BertForMaskedLM
 import torch
@@ -20,26 +21,87 @@ import wandb
 from tqdm.auto import tqdm
 from decimal import Decimal
 import datetime, json
+import webdataset as wds
+import random
+import open_clip
+from clip.model import CLIP
 
-class MLMTrainer:
-    def __init__(self, tokenizer, model, args):
+
+class PreTrainer:
+    def __init__(self, tokenizer, model, args, image_processor=None):
         self.tokenizer = tokenizer
         self.model = model
         self.args = args
         self.optimizer = AdamW(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
         self.lr_scheduler = get_scheduler("linear",optimizer=self.optimizer,num_warmup_steps=self.args.warmup_steps,
                                           num_training_steps=self.args.total_steps)
-        self.train_dataloader, self.val_dataloader = self.load_data(self.args.data_path, self.args.batch_size, self.args.num_proc)
-        self.accelerator = accelerate.Accelerator(split_batches=True) #grad acc and precision (mixed/bf16) in deepspeed config
+
+        if self.args.clip:
+            self.train_image_processor, self.val_image_processor = image_processor
+            self.loss_fn = open_clip.loss.ClipLoss()
+
+
+        self.accelerator = accelerate.Accelerator(split_batches=True) # change grad acc and precision (mixed/bf16) in deepspeed config
+
+        if self.args.mlm:
+            self.train_dataloader, self.val_dataloader = self.load_data_for_mlm(self.args.data_path, self.args.batch_size, self.args.num_proc)
+        elif args.clip:
+            self.train_dataloader, self.val_dataloader = self.clip_webdataset_reader(self.args.data_path, self.args.batch_size, num_proc=self.args.num_proc)
+
         self.model, self.optimizer, self.lr_scheduler, self.train_dataloader, self.val_dataloader =  self.accelerator._prepare_deepspeed(
             self.model, self.optimizer, self.lr_scheduler, self.train_dataloader, self.val_dataloader
         )
-        self.experiment_name = (f'{self.args.model_name}_mrl{self.args.mrl}{self.args.mrl_efficient}_'
+
+        self.dtype = self.model.get_data_types()[0]
+
+        self.experiment_name = (f'{self.args.model_name.split("/")[-1]}_mrl{self.args.mrl}{self.args.mrl_efficient}_'
                                 f'bs{self.args.batch_size}_lr{"%.1E"%Decimal(self.args.lr)}_warmup{self.args.warmup_steps}')
 
+    def clip_batch_reader_fn(self,data, is_train):
+        image_batch, text_batch = [], []
+
+        if is_train:
+            image_processor = self.train_image_processor
+        else:
+            image_processor = self.val_image_processor
+
+        for i, sample in enumerate(data):
+            image_batch.append(image_processor(sample['jpg']))
+            text_batch.append(sample['json']['caption'])
+
+        image_tensors = torch.stack(image_batch)
+        tokenized_tensors = self.tokenizer(text_batch)
+
+        image_tensors = image_tensors.to(self.dtype)
+
+        output_dict = {
+            'image':image_tensors,
+            'text': tokenized_tensors
+        }
+        return output_dict
+
+    def clip_webdataset_reader(self, location, batch_size, num_proc):
+        location_shards = os.listdir(location)
+        location_shards = [location + shard for shard in location_shards if shard[-4:] == '.tar']
+        random.shuffle(location_shards)
+
+        val_shards = location_shards[:len(location_shards) // 10]
+        train_shards = location_shards[len(location_shards)//10:]
+
+        train_dataset = wds.WebDataset(train_shards).decode('pil')
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                                       collate_fn=lambda x: self.clip_batch_reader_fn(x, is_train=True),
+                                                       num_workers=num_proc//2)
+
+        val_dataset = wds.WebDataset(val_shards).decode('pil')
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size,
+                                                     collate_fn=lambda x: self.clip_batch_reader_fn(x, is_train=False),
+                                                     num_workers=num_proc//4)
+
+        return train_dataloader, val_dataloader
 
 
-    def load_data(self, data_path, batch_size, num_proc=16):
+    def load_data_for_mlm(self, data_path, batch_size, num_proc=16):
         dataset = load_from_disk(data_path).with_format('torch')
         dataset = dataset.rename_columns({'masked_input_ids':'input_ids',
                                            'masked_attention_mask': 'attention_mask',
@@ -59,10 +121,6 @@ class MLMTrainer:
     def train(self):
 
         self.model.train()
-
-
-
-
         self.accelerator.register_for_checkpointing(self.lr_scheduler)
         print(self.args)
         out_dir = f'{self.args.output_path}/{self.experiment_name}'
@@ -93,7 +151,12 @@ class MLMTrainer:
         for iter, batch in zip(range(init_step, self.args.total_steps), self.train_dataloader):
 
             outputs = model(**batch)
-            loss = outputs.loss
+            if self.args.clip:
+                losses = self.loss_fn(**outputs)
+                loss = losses.sum()
+            elif self.args.mlm:
+                loss = outputs.loss
+
             self.accelerator.backward(loss)
             self.optimizer.step()
             self.lr_scheduler.step()
@@ -117,8 +180,12 @@ class MLMTrainer:
                         break
                     with torch.no_grad():
                         outputs = self.model(**batch)
-                        loss = outputs.loss
-                        losses.append(self.accelerator.gather(loss.repeat(batch['input_ids'].shape[0])))
+                        if self.args.clip:
+                            losses = self.loss_fn(**outputs)
+                            loss = losses.sum()
+                        elif self.args.mlm:
+                            loss = outputs.loss
+                            losses.append(self.accelerator.gather(loss.repeat(batch['input_ids'].shape[0])))
                         eval_progress.update(1)
 
 
@@ -129,7 +196,11 @@ class MLMTrainer:
                     losses = losses[:len(self.val_dataloader)]
                     mean_loss = torch.mean(losses)
                     perplexity = math.exp(mean_loss)
-                    wandb.log({'perplexity': perplexity, 'val_loss': mean_loss.item()})
+                    if self.args.mlm:
+                        logging_dict = {'perplexity': perplexity, 'val_loss': mean_loss.item()}
+                    elif self.args.clip:
+                        logging_dict = {'val_loss': mean_loss.item()}
+                    wandb.log(logging_dict)
                     self.print_time_log(iter, progress_bar)
 
                     #unwrap_model = self.accelerator.unwrap_model(self.model)
@@ -175,6 +246,8 @@ if __name__ == '__main__':
     parser.add_argument('--output_path', type=str, help='path to save logs and model weights', default='/tk/output_dir')
     parser.add_argument('--resume_train_from', type=str, help='path to saved checkpoint to resume training', default=None)
     parser.add_argument('--model_name', type=str, default='bert-base-uncased')
+    parser.add_argument('--mlm', type=str2bool, default=False, help='to trigger MLM pretraining')
+    parser.add_argument('--clip', type=str2bool, default=True, help='to trigger CLIP pretraining')
     parser.add_argument('--mrl', type=str2bool, default=False, help='whether to use MRL')
     parser.add_argument('--mrl_efficient', type=str2bool, default=False)
     parser.add_argument('--nesting_dim', type=str2list, default=[96,192,384,768])
@@ -186,26 +259,40 @@ if __name__ == '__main__':
     parser.add_argument('--num_proc', type=int, default=multiprocessing.cpu_count(), help='number of processes')
     parser.add_argument('--evaluation_interval', type=int, default=25000)
     parser.add_argument('--wandb_key', type=str, default=None, help='wandb api login key for remote login')
+    parser.add_argument('--seed', type=int, default=42)
     args = parser.parse_args()
 
+
+    random.seed(args.seed)
     if args.wandb_key:
         wandb.login(key=args.wandb_key)
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name.lower())
+
+    if args.mlm:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name.lower())
+        config = AutoConfig.from_pretrained(args.model_name.lower())
+        if args.mrl:
+            config.nesting_dim = args.nesting_dim
+        else:
+            config.nesting_dim = None
+        config.mrl_efficient = args.mrl_efficient
+
+        model = BertForMaskedLM(config=config)
+        trainer = PreTrainer(tokenizer, model, args)
+    elif args.clip:
 
 
-    config = AutoConfig.from_pretrained(args.model_name.lower())
-    if args.mrl:
-        config.nesting_dim = args.nesting_dim
-    else:
-        config.nesting_dim = None
-    config.mrl_efficient = args.mrl_efficient
+
+        tokenizer = open_clip.get_tokenizer(args.model_name)
+        config = open_clip.get_model_config(args.model_name)
+
+        model, train_image_processor, val_image_processor = open_clip.create_model_and_transforms(args.model_name)
+        model = CLIP(**config, cast_dtype=torch.bfloat16)
+        trainer = PreTrainer(tokenizer, model, args, image_processor=(train_image_processor, val_image_processor))
 
 
-    model = BertForMaskedLM(config=config)
 
 
-    trainer = MLMTrainer(tokenizer, model, args)
     trainer.train()
 
 
