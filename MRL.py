8a6b73742bf79b6535fedc6f7bf16a3f56050d79
Tuhin Kundu <tuhinkundu@outlook.com>
2024-03-07 22:28:14 -0800

    clip loss (#2)
    need to merge this for training

--- MRL.py
+++ MRL.py
@@ -3,15 +3,33 @@
 import torch
 import torch.nn as nn
 from typing import Type, Any, Callable, Union, List, Optional
-
+from open_clip.loss import ClipLoss
 '''
 Loss function for Matryoshka Representation Learning 
 '''
 
 
-class Matryoshka_CE_Loss(nn.Module):
+class MatryoshkaClipLoss(ClipLoss):
+    def __init__(self, nesting_dim, loss_weights, **kwargs):
+        super().__init__(**kwargs)
+        self.nesting_dim = nesting_dim
+
+        self.loss_weights = loss_weights
+
+    def forward(self, image_features, text_features, logit_scale, output_dict=False):
+        total_loss = 0
+        for i, dim in enumerate(self.nesting_dim):
+            img_dim_features =  torch.nn.functional.normalize(image_features[:,:dim], dim=-1)
+            text_dim_features = torch.nn.functional.normalize(text_features[:,:dim], dim=-1)
+            loss = super().forward(img_dim_features, text_dim_features, logit_scale)
+            total_loss += loss * self.loss_weights[i]
+
+        return total_loss
+
+
+class Matryoshka_MLM_Loss(nn.Module):
     def __init__(self, vocab_size: int, relative_importance: List[float] = None, **kwargs):
-        super(Matryoshka_CE_Loss, self).__init__()
+        super(Matryoshka_MLM_Loss, self).__init__()
         self.criterion = nn.CrossEntropyLoss(**kwargs)
         self.vocab_size = vocab_size
         # relative importance shape: [G]
@@ -34,10 +52,10 @@ class Matryoshka_CE_Loss(nn.Module):
 
 
 class MRL_Linear_Layer(nn.Module):
-    def __init__(self, nesting_list: List, out_dim: int, efficient=False, **kwargs):
+    def __init__(self, nesting_list: List, vocab_size: int, efficient=False, **kwargs):
         super(MRL_Linear_Layer, self).__init__()
         self.nesting_list = nesting_list
-        self.out_dim = out_dim
+        self.out_dim = vocab_size
         self.efficient = efficient
         if self.efficient:
             setattr(self, f"nesting_classifier_{0}", nn.Linear(nesting_list[-1], self.out_dim, **kwargs))
@@ -68,20 +86,3 @@ class MRL_Linear_Layer(nn.Module):
                 nesting_logits += (getattr(self, f"nesting_classifier_{i}")(x[:, :,:num_feat]),)
 
         return nesting_logits
-
-
-class FixedFeatureLayer(nn.Linear):
-    '''
-    For our fixed feature baseline, we just replace the classification layer with the following.
-    It effectively just look at the first "in_features" for the classification.
-    '''
-
-    def __init__(self, in_features, out_features, **kwargs):
-        super(FixedFeatureLayer, self).__init__(in_features, out_features, **kwargs)
-
-    def forward(self, x):
-        if not (self.bias is None):
-            out = torch.matmul(x[:, :self.in_features], self.weight.t()) + self.bias
-        else:
-            out = torch.matmul(x[:, :self.in_features], self.weight.t())
-        return out
