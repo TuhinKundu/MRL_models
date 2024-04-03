5c6ef9dcd29fe0642cefec619cc0f674cc5bffbf
Tuhin Kundu <tuhinkundu@outlook.com>
2024-04-02 21:21:35 -0700

    Open clip mrl (#3)
    * open clip implementation
    
    * open clip implementation
    
    * delete logs
    
    * add wandb_key argument
    
    * wandb arg
    
    * delete jobs
    
    * adding accelerate deepspeed to open_clip
    
    * changing model name for wandb
    
    * adding shell
    
    * adding configs and fixing wandb issues
    
    * prec related bugs + doesnt yet support bf16 due to layernorm not support CUDABfloat16Type
    
    * ds doesnt handle grad acc
    
    * enabled back bf16, fix layernorm bug, keep fp16 ds config for future
    
    * removing a print statement

---
+++ open_clip_mrl/scripts/mrl_clip.sh
@@ -0,0 +1,24 @@
+cd /mmfs1/gscratch/krishna/arnabk1/open_clip_mrl/src
+torchrun --nproc_per_node 4 --master_port 4321 -m training.main \
+    --model "ViT-B-16" \
+    --train-data "/gscratch/krishna/chenhaoz/IL/FDT/data/cc3m/{00000..00331}.tar"\
+    --imagenet-val "/gscratch/krishna/arnabk1/root/val/" \
+    --dataset-type webdataset \
+    --precision amp \
+    --gather-with-grad \
+    --local-loss \
+    --force_mrl_loss \
+    --mrl_loss_weights "1,1,1,1,1" \
+    --mrl_dim_to_consider "768,384,192,96,48" \
+    --batch-size 256 \
+    --train-num-samples 3308333 \
+    --accum-freq 32 \
+    --workers 4 \
+    --epochs 40 \
+    --warmup 1000 \
+    --zeroshot-frequency 1 \
+    --seed 42 \
+    --report-to 'wandb' \
+    --wandb-project-name "mrl_clip_training" \
+    --logs "/mmfs1/gscratch/krishna/arnabk1/mayank_clip_mrl/scripts/logs/" \
+    --name "mrl_clip_cc3m_b256_accum_32_gpu_4_ep40_diffLogitScale_D032824_w1" 
